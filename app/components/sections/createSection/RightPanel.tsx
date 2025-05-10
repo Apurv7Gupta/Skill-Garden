a490227e130ca14f76d6705daf8cb9e8b67e6c53
Apurv7Gupta <apurvgupta.2017@gmail.com>
2025-05-10 22:28:10 +0530

    Update Panels

--- app/components/sections/createSection/RightPanel.tsx
+++ app/components/sections/createSection/RightPanel.tsx
@@ -79,73 +79,81 @@ export default function RightPanel() {
       </Box>
 
       {/* {Button container} */}
-
       <Box
         sx={{
-          m: 2,
-          p: { xs: 1, sm: 2, md: 3 },
           display: "flex",
-          justifyContent: "center",
-          gap: { xs: 1, sm: 2 },
-          bgcolor: "lavender",
-          borderRadius: 2,
-          flexWrap: "nowrap", // stay in one row
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignContent: "center",
         }}
       >
-        <Button
-          variant="contained"
-          color="success"
+        <Box
           sx={{
-            fontSize: {
-              xs: "0.3rem",
-              sm: "0.4rem",
-              md: "0.6rem",
-              lg: "0.9rem",
-            },
-
-            px: {
-              xs: 1,
-              sm: 2,
-              md: 3,
-            },
-            py: {
-              xs: 0.25,
-              sm: 0.5,
-              md: 1,
-            },
-            minWidth: 0,
-            flexShrink: 1,
+            m: 2,
+            p: { xs: 1, sm: 2, md: 3 },
+            display: "flex",
+            justifyContent: "center",
+            gap: { xs: 1, sm: 2 },
+            bgcolor: "lavender",
+            borderRadius: 2,
+            flexWrap: "nowrap", // stay in one row
           }}
         >
-          Add Node
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          sx={{
-            fontSize: {
-              xs: "0.3rem",
-              sm: "0.4rem",
-              md: "0.6rem",
-              lg: "0.9rem",
-            },
+          <Button
+            variant="contained"
+            color="success"
+            sx={{
+              fontSize: {
+                xs: "0.3rem",
+                sm: "0.4rem",
+                md: "0.6rem",
+                lg: "0.9rem",
+              },
 
-            px: {
-              xs: 1,
-              sm: 2,
-              md: 3,
-            },
-            py: {
-              xs: 0.25,
-              sm: 0.5,
-              md: 1,
-            },
-            minWidth: 0,
-            flexShrink: 1,
-          }}
-        >
-          Add Branch
-        </Button>
+              px: {
+                xs: 1,
+                sm: 2,
+                md: 3,
+              },
+              py: {
+                xs: 0.25,
+                sm: 0.5,
+                md: 1,
+              },
+              minWidth: 0,
+              flexShrink: 1,
+            }}
+          >
+            Add Node
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            sx={{
+              fontSize: {
+                xs: "0.3rem",
+                sm: "0.4rem",
+                md: "0.6rem",
+                lg: "0.9rem",
+              },
+
+              px: {
+                xs: 1,
+                sm: 2,
+                md: 3,
+              },
+              py: {
+                xs: 0.25,
+                sm: 0.5,
+                md: 1,
+              },
+              minWidth: 0,
+              flexShrink: 1,
+            }}
+          >
+            Add Branch
+          </Button>
+        </Box>
       </Box>
     </>
   );
